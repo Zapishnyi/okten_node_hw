a63f9b9ec5d425524fdf934f417cde3ea6dbfb57
Zapishnyi <zapishny@ukr.net>
2024-08-01 21:06:20 +0300

    Lesson 9 | Cron job add

--- src/crons/userNoActivity.ts
+++ src/crons/userNoActivity.ts
@@ -11,13 +11,13 @@ const UserNoActivity = async () => {
     const noActivityUsers = await userRepository.findManyByParam({
       createdAt: { $lte: dayjs().subtract(5, "days").toDate() },
     });
-    if (noActivityUsers) {
-      noActivityUsers.forEach((user) => {
-        emailService.sendEmail(EmailTypeEnum.REMIND, user.email, {
+    if (noActivityUsers?.length) {
+      for (const user of noActivityUsers) {
+        await emailService.sendEmail(EmailTypeEnum.REMIND, user.email, {
           name: user.userName,
           frontUrl: config.FRONT_END_URL,
         });
-      });
+      }
     }
   } catch (err) {
     console.log(`${this} crone failed with error:`, err);
