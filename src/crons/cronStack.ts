9c37d70f378b7bc497312e269848af8bfefbc748
Zapishnyi <zapishny@ukr.net>
2024-08-01 19:49:17 +0300

    Lesson 9 | Cron job add

--- src/crons/cronStack.ts
+++ src/crons/cronStack.ts
@@ -1,7 +1,9 @@
 import { deleteExpiredAccessPairTokens } from "./deleteExpiredAccessPairTokens.cron";
 import { deleteOldPasswords } from "./deleteOldPasswords.cron";
+import { userNoActivity } from "./userNoActivity";
 
 export const jobRunner = () => {
   deleteExpiredAccessPairTokens.start();
   deleteOldPasswords.start();
+  userNoActivity.start();
 };
